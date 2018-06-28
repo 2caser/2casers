aa60b4bf0315ba201cb204321744f7489f7a443e
Anita Solomon <2Anita@Anitas-MBP.CARRDN>
2018-06-28 09:35:39 -0700

    add file.py

---
+++ file.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+# path = '/Users/2Anita/Downloads/FL_insurance_sample.csv'
+csvFile = open('/Users/2Anita/Documents/Anita/FL_insurance_sample.csv', 'r+')
+print csvFile.read()
+'''
+for line in csvFile:
+    #csvFile.write("TESTin,TESTin,TESTin,TESTin,TESTin,TESTin,TESTin\nAGAIN,AGAIN,AGAIN,AGAIN,AGAIN\nIS,IS,IS,IS\nFUN,FUN")
+    print (line)
+
+'''
+
+
+print csvFile.read()
